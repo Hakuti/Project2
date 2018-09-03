f358805d1bc63b96b78248cd5cf8cef0a45af8c0
Jonathan <sellsj14@gmail.com>
2018-09-03 15:39:17 -0400

    just updating

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+      res.render("index")
   });
 
   app.get("/login", function(req, res) {
