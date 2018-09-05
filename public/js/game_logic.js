17eccccc82fcf163e6a8f76b2f58f3284ea2ca1a
Jonathan <sellsj14@gmail.com>
2018-09-05 18:35:10 -0400

    everything working

--- public/js/game_logic.js
+++ public/js/game_logic.js
@@ -1,26 +1,5 @@
 //find all the artist of that genre and build the array
 //jquery grab drop menu genre selection then ajax 
-$(document).ready(function(){
 
-    $(".dropdown-item").on("click",function(){
-        var genre = $(this).attr("data-genre");
-        console.log(genre);
-        //now lets pass this data somewhere that matters
-        //when the start button
-        $("#start_game").on("click", function(){
-            console.log("hello " + genre);
-            //send the genre as a route param; /api/game/:genre
-            launch(genre);
-        })
-    });
-
-    function launch(test){
-        //just locally storing the genre that was selected by the user
-        //will be accessed on game launch on next page
-        localStorage.setItem("genre", test);
-        console.log("stored");
-    }
-
-});
 
 
--- public/js/login.js
+++ public/js/login.js
@@ -33,4 +33,29 @@ $(document).ready(function() {
     });
   }
 
+
+  $(document).ready(function(){
+
+    $(".dropdown-item").on("click",function(){
+        var genre = $(this).attr("data-genre");
+        console.log(genre);
+        //now lets pass this data somewhere that matters
+        //when the start button
+        $("#start_game").on("click", function(){
+            console.log("hello " + genre);
+            //send the genre as a route param; /api/game/:genre
+            launch(genre);
+        })
+    });
+
+    function launch(test){
+        console.log(test);
+        //just locally storing the genre that was selected by the user
+        //will be accessed on game launch on next page
+        localStorage.setItem("genre", test);
+        console.log("stored");
+    }
+
+});
+
 });
