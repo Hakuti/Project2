c9e55f170a2c8abded2a31a64ed96086f05aac86
Jonathan <sellsj14@gmail.com>
2018-09-04 22:46:03 -0400

    added few extra comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -48,6 +48,9 @@ module.exports = function(app) {
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
     //res.json("/members");
+
+    //actually don't reroute to anywhere, the login is noted in the db and then the login/signup box is 
+    //replaced with a drop down menu
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
