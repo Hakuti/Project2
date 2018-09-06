0d6822d783f2ef1997687f22dfc4500856b821f5
Jonathan <sellsj14@gmail.com>
2018-09-05 21:06:17 -0400

    start that modal

--- public/js/game_logic.js
+++ public/js/game_logic.js
@@ -1,5 +1,7 @@
 //find all the artist of that genre and build the array
 //jquery grab drop menu genre selection then ajax 
 
-
+$(document).ready(function(){
+    $("#start_modal").modal("show");
+});
 
